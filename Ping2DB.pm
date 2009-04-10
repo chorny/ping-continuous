26a9b6bb42eab8c0d6f8338f1e4f4583f631fc9c
Alexandr Ciornii <alexchorny@gmail.com>
2009-04-10 15:50:42 +0300

    add create_sqlite_db

---
+++ Ping2DB.pm
@@ -0,0 +1,22 @@
+package Ping2DB;
+use 5.006;
+use strict;
+use warnings;
+use DBI;
+use Time::Piece;
+use Exporter 'import';
+
+sub create_sqlite_db {
+  my $dbh = DBI->connect('DBI:SQLite:ping_db.sqlite','','')
+    || die('Connect error: '.$DBI::errstr);
+  $dbh->do(<<'EOT') or die('DB error '. $dbh->errstr());
+CREATE TABLE pings_by_hour (
+ dt VARCHAR NOT NULL PRIMARY KEY,
+ pings_pass INTEGER NOT NULL DEFAULT 0,
+ pings_fail INTEGER NOT NULL DEFAULT 0
+)
+EOT
+  return $dbh;
+}
+
+1;
